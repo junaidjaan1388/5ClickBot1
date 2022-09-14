d7e1469095afce6c76dc0f67f2bf3579f9dd9f00
junaidjaan1388 <112433148+junaidjaan1388@users.noreply.github.com>
2022-09-15 00:02:17 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ python bot.py +[+996554520007] ltc "MBoruBKq5GPrsudzv2GdDdXvy3PPe6i881"
 python bot.py +[+996554520007] btc "3Hvi4zaxu86s3ZT7z9b4TJweAGmc9sbqnM"
 python bot.py +[+996554520007] bch "bitcoincash:qqkwyysnzm20r9gm48dxhfst0x3wjy9cpcklepg09l"
 clear
-pwd
+#pwd
 bash start.sh
 
 #Replace [phone] with your Number Phone
